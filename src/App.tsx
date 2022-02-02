6b0875e7b54da3c6b7463ed0f4c2e26e9686f446
Alisson Fredo <douglasalissonsf@gmail.com>
2022-02-02 01:52:46 -0300

    adding styled component and GlobalStyled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
         hello word
+        <GlobalStyle />
     </div>
   );
 }
